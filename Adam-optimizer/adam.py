69199d2bd8d49f4719d3157dfd80af8af4d59a18
Gio <cmurogio@gmail.com>
2021-02-23 19:54:43 +0100

    added comments

--- Adam-optimizer/adam.py
+++ Adam-optimizer/adam.py
@@ -4,6 +4,7 @@ import torch
 import torch.optim.functional as F
 from torch.optim import Optimizer
 
+# Inherit from Optimizer class which handles all the general optimizer functionalities
 class Adam(Optimizer):
     """
     Implements Adam algorithm.
@@ -29,6 +30,7 @@ class Adam(Optimizer):
     """
 
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0, amsgrad=False):
+        # Handle invalid attribute values
         if not 0.0 <= lr:
             raise ValueError(f"Invalid learning rate: {lr}")
         if not 0.0 <= eps:
@@ -40,6 +42,7 @@ class Adam(Optimizer):
         if not 0.0 <= weight_decay:
             raise ValueError(f"Invalid weight_decay value: {weight_decay}")
 
+        # Init default dictionary
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_dacay=weight_decay, amsgrad=amsgrad)
 
         super(Adam, self).__init__(params, defaults)
@@ -49,19 +52,24 @@ class Adam(Optimizer):
         for group in self.param_groups:
             group.setdefault('amsgrad', False)
 
-    @torch.no_grad()
+    @torch.no_grad() # No grad decorator
     def step(self, closure=None):
         """
         Performs a single optimization step.
            Args:
-            --> closure (callable, optional): A closure that reevaluates the model and returns the loss.
+             --> closure (callable, optional): A closure that reevaluates the model and returns the loss.
         """
+        # Init loss
         loss = None
+
+        # Check if the closure is already defined
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
 
+        # Iterate through the parameters. A parameter is an iterable that specifies what Tensors should be optimized.
         for group in self.param_groups:
+            # Init all the variables that we need to do the computation
             params_with_grad = []
             grads = []
             exp_avgs = []
@@ -70,15 +78,20 @@ class Adam(Optimizer):
             max_exp_avg_sqs = []
             state_steps = []
 
+            # Iterate over the tensors in the current parameter
             for p in group['params']:
+                # Check if the tensor's gradient is already defined
                 if p.grad is not None:
                     params_with_grad.append(p)
+                    # Check if the tensor's gradient is sparse
                     if p.grad.is_sparse:
                         raise RuntimeError('Adam does not support sparse gradients, please consideer SparseAdam instead')
+
                     grads.append(p.grad)
 
+                    # Get the current optimizer state
                     state = self.state[p]
-                    # Lazy state initialization
+                    # Lazy state initialization -> check if it's the first time we access the state, if it is then we need to set some attributes
                     if len(state) == 0:
                         state['step'] = 0
                         # Exponential moving average of gradient values
@@ -99,6 +112,8 @@ class Adam(Optimizer):
                     state_steps.append(state['step'])
 
             beta1, beta2 = group['betas']
+
+            # Functional API that performs Adam algorithm computation
             F.adam(
                 params=params_with_grad,
                 grads=grads,
