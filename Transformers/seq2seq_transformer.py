018d25b27793db97752a0920118effa69a812ae4
Gio <cmurogio@gmail.com>
2021-03-02 22:17:26 +0100

    added comments

--- Transformers/seq2seq_transformer.py
+++ Transformers/seq2seq_transformer.py
@@ -22,34 +22,61 @@ from torchtext.vocab import build_vocab_from_iterator
 # the final Linear layer, which is followed by a log-Softmax function.
 
 class TransformerModel(nn.Module):
-    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
+    def __init__(
+        self, 
+        ntoken,      # Size of vocabulary
+        ninp,        # Embedding dimension
+        nhead,       # Number of heads in the multiheadattention models
+        nhid,        # Dimension of the feedforward network model
+        nlayers,     # Number of transformer encoder layers
+        dropout=0.5  # Dropout rate
+    ):
         super(TransformerModel, self).__init__()
-
+        # Init model type
         self.model_type = 'Transformer'
+        # Get positional encodings
         self.pos_encoder = PositionalEncoding(ninp, dropout)
-        encoder_layers = nn.TransformerEncoderLayer(ninp, nhead, nhid, dropout)
+        # Define transformer encoder -> contains multi-head attension, feed forward network and normalization
+        encoder_layers = nn.TransformerEncoderLayer(
+            d_model=ninp,          # Number of expected features in the input
+            nhead=nhead,           # Number of heads in the multiheadattention models
+            dim_feedforward=nhid,  # Dimension of the feedforward network model
+            dropout=dropout        # Dropout rate
+        )
         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, nlayers)
-        self.encoder = nn.Embedding(ntoken, ninp)
+        # Define embedding layer
+        self.encoder = nn.Embedding(num_embeddings=ntoken, embedding_dim=ninp)
+        # Init expected features
         self.ninp = ninp
-        self.decoder = nn.Linear(ninp, ntoken)
+        # Define decoder
+        self.decoder = nn.Linear(in_features=ninp, out_features=ntoken)
 
+        # Initialize weights
         self.init_weights()
     
+    # Defines function to mask the yet to predict words in the output sequence
     def generate_square_subsequent_mask(self, sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, flaot(0.0))
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)  # Returns the part of the matrix above the diagonal and then it transposes the matrix
+        mask = mask.float()
+               .masked_fill(mask == 0, float('-inf'))   # Fills the mask tensor with '-inf' where mask == 0
+               .masked_fill(mask == 1, flaot(0.0))      # Fills the mask tensor with '0.0' where mask == 1
         return mask
 
+    # Defines the weights initialization
     def init_weights(self):
         init_range = 0.1
-        self.encoder.weight.data.uniform_(-init_range, init_range)
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-init_range, init_range)
+        self.encoder.weight.data.uniform_(-init_range, init_range)  # Uniform encoder's weights in the range passed
+        self.decoder.bias.data.zero_()  # Set decoder's bias to zero
+        self.decoder.weight.data.uniform_(-init_range, init_range)  # Uniform decoder's weights' values in the range passed
 
     def forward(self, src, src_mask):
+        # Pass data into the embeddings and normalize with the square of the embedding dimensions
         src = self.encoder(src) * math.sqrt(self.ninp)
+        # Get positional encodings
         src = self.pos_encoder(src)
+        # Pass the vector into the transformer encoder
         output = self.transformer_encoder(src, src_mask)
+        # Pass the output of the transformer encoder into the decoder 
         output = self.decoder(output)
         return output
 
@@ -57,18 +84,27 @@ class TransformerModel(nn.Module):
 
 # PositionalEncoding module injects information about the relative or absolute position of the tokens in the sequence. 
 # The positional encodings have the same dimension as the embeddings so that the two can be summed.
-
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(
+            self, 
+            d_model,      # Define the number of expected features (embedding dimension)
+            dropout=0.1,  # Define dropout rate
+            max_len=5000  # Define the max lenght of the sequence
+        ):
         super(PositionalEncoding, self).__init__()
 
         self.dropout = nn.Dropout(p=dropout)
-        pe = torch.zeros(max_len,  d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0,  d_model, 2).float() *  (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len,  d_model)  # Returns a tensor filled with 0, of "max_len" shape and output "d_model"
+        position = torch.arange(start=0, end=max_len, dtype=torch.float).unsqueeze(1)  # Returns a tensor of size "max_len" with values between [0, max_len) and with dimension 1
+        div_term = torch.exp(
+            torch.arange(start=0,  end=d_model, step=2).float() *  (-math.log(10000.0) / d_model)
+        ) # Returns the the exponential divisor used to calculate the distance between words
+
+        # Calculate sinusoidal position encoding
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
+        # Add a buffer to the module
         self.register_buffer('pe', pe)
 
     def forward(self, x):
@@ -79,16 +115,20 @@ class PositionalEncoding(nn.Module):
 
 # ----------------------------------- LOAD AND BATCH DATA ----------------------------------- #
 
-
+# Get data through URL
 url = 'https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip'
 test_filepath, valid_filepath, train_filepath = extract_archive(download_from_url(url))
+# Define tokenizer
 tokenizer = get_tokenizer('basic_english')
+# Define vocabulary
 vocab = build_vocab_from_iterator(map(tokenizer, iter(io.open(train_filepath, encoding='utf8'))))
 
 def data_process(raw_text_iter):
+    # Get the corresponding index from the vocabulary for each sentence in the text after tokenization
     data = [torch.tensor([vocab[token] for token in tokenizer(item)], dtype=torch.long) for item in raw_text_iter]
     return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
 
+# Get training, validation and test datasets
 train_data = data_process(iter(io.open(train_filepath, encoding='utf8')))
 val_data = data_process(iter(io.open(valid_filepath, encoding='utf8')))
 test_data = data_process(iter(io.open(test_filepath, encoding='utf8')))
@@ -108,6 +148,7 @@ def batchify(data, bsz):
 
 batch_size = 20
 eval_batch_size = 10
+
 train_data = batchify(train_data, batch_size)
 val_data = batchify(val_data, eval_batch_size)
 test_data = batchify(test_data, eval_batch_size)
@@ -150,27 +191,41 @@ scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
 def train():
     # Turn the model on training mode
     model.train()
+    # Init total loss and starting time
     total_loss = 0
     start_time = time.time()
+    # Generate the mask
     src_mask = model.generate_square_subsequent_mask(bptt).to(device)
 
+    # Iterate throught the batch
     for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        # Get examples and targets
         data, targets = get_batch(train_data, i)
+        # Set gradients to zero
         optimizer.zero_grad()
+        # Check if the size of the data is different from the previously initialiated chucks' length
         if data.size(0) != bptt:
             src_mask = model.generate_square_subsequent_mask(data.size(0)).to(device)
+        # Get output from model
         output = model(data, src_mask)
+        # Calculate loss
         loss = criterion(output.view(-1, n_tokens), targets)
+        # Calculate gradients
         loss.backward()
         # Scale all the gradients together to prevent exploding
         torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        # Update parameters
         optimizer.step()
 
+        # Calculate total loss
         total_loss += loss.item()
+
         log_interval = 200
         
         if batch % log_interval == 0 and batch > 0:
+            # Current loss
             cur_loss = total_loss / log_interval
+            # Time passed
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | '
                   'lr {:02.2f} | ms/batch {:5.2f} | '
@@ -179,23 +234,31 @@ def train():
                     elapsed * 1000 / log_interval,
                     cur_loss, math.exp(cur_loss))
             )
+            # Reset loss and time
             total_loss = 0
             start_time = time.time()
 
 def evaluate(eval_model, data_source):
     # Turn the model on evaluation mode
     eval_model.eval()
+    # Init total loss
     total_loss = 0
+    # Generate the mask
     src_mask = model.generate_square_subsequent_mask(bptt).to(device)
 
     with torch.no_grad():
         for i in range(0, data_source.size(0) - 1, bptt):
+            # Get examples and targets
             data, targets = get_batch(data_source, i)
+            # Check if the size of the data is different from the previously initialiated chucks' length
             if data.size(0) != bptt:
                 src_mask = model.generate_square_subsequent_mask(data.size(0)).to(device)
+            # Get output from model
             output = eval_model(data, src_mask)
             output_flat = output.view(-1, n_tokens)
+            # Calculate total loss
             total_loss += len(data) * criterion(output_flat, targets).item()
+
     return total_loss / (len(data_source) - 1)
 
 
@@ -205,21 +268,28 @@ epochs = 3
 best_model = None
 
 for epoch in  range(1, epochs + 1):
+    # Init starting time
     epoch_start_time = time.time()
+    # Train
     train()
+    # Evaluate model on validation set
     val_loss = evaluate(model, val_data)
     print('-' * 90)
     print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time), val_loss, math.exp(val_loss)))
     print('-' * 90)
 
+    # Save model to "best_model" if it's the best until now
     if val_loss < best_val_loss:
         best_val_loss = val_loss
         best_model = model
 
+    # Update the learning Rate scheduler
     scheduler.step()
 
 # EVALUATE MODEL #
+# Evaluate model on test set
 test_loss = evaluate(best_model, test_data)
+
 print('=' * 90)
 print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(test_loss, math.exp(test_loss)))
 print('=' * 90)
