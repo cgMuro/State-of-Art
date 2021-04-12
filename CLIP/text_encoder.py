f0d854179e2172184967e0df1cb19c08b2829833
Gio <cmurogio@gmail.com>
2021-04-12 20:10:21 +0200

    added type handling for input, changed mask logic and fixed bug in parameters initialization

--- CLIP/text_encoder.py
+++ CLIP/text_encoder.py
@@ -13,13 +13,15 @@ class TransformerTextEncoder(nn.Module):
         n_blocks: int = 12,        # Number of blocks that compose the transformer (i.e. the depth) [possible values from the paper: 12, 16]
         n_heads: int = 8,          # Number of heads for each multi-head attention layer [possible values from the paper: 8, 12]
         head_dim: int = 64,        # Dimension of each multi-head layer
-        dropout: float = 0.5       # Define dropout
+        dropout: float = 0.5,      # Define dropout
+        tensor_type: torch.TensorType = torch.float # Define type of tensor to convert input data to
     ):
         super().__init__()
 
         self.max_length = max_length
         self.n_blocks = n_blocks
         self.width = width
+        self.tensor_type = tensor_type
 
         # Define mask
         mask = self.get_mask()
@@ -43,7 +45,7 @@ class TransformerTextEncoder(nn.Module):
     def get_mask(self):
         """ Function that creates the mask for the transformer """
         mask = torch.empty(self.max_length, self.max_length)  # Create empty mask
-        mask.fill_(float('-inf'))  # Fill the entire mask with -inf
+        # mask.fill_(float('-inf'))  # Fill the entire mask with -inf
         mask.triu_(diagonal=1)  # Zero out the diagonal and the entries under the diagonal
         return mask
 
@@ -59,7 +61,7 @@ class TransformerTextEncoder(nn.Module):
         fc_std = (2 * self.width) ** -0.5
         for attention, feedforward in self.transformer.layers:
             nn.init.normal_(attention.to_qkv.weight, std=attn_std)
-            nn.init.normal_(attention.to_out.weight, std=proj_std)
+            nn.init.normal_(attention.to_out[0].weight, std=proj_std)
             nn.init.normal_(feedforward.net[0].weight, std=fc_std)
             nn.init.normal_(feedforward.net[3].weight, std=proj_std)
 
@@ -71,14 +73,14 @@ class TransformerTextEncoder(nn.Module):
         idx_max_n = x.argmax(dim=-1)
         
         # Apply token and positional embeddings
-        x = self.token_embedding(x).type(x.dtype)   # shape = [batch_size, n_ctx, d_model]
-        x = x + self.positional_embedding(x).type(x.dtype)
+        x = self.token_embedding(x).type(self.tensor_type)   # shape = [batch_size, n_ctx, d_model]
+        x = x + self.positional_embedding.type(self.tensor_type)
         x = x.permute(1, 0, 2)
 
         # Apply transformer, permute and normalize
         x = self.transformer(x)
         x = x.permute(1, 0, 2)  # shape = [batch_size, n_ctx, transformer.width]
-        x = self.layernorm(x).type(x.dtype)
+        x = self.layernorm(x).type(self.tensor_type)
 
         # Take features from the eot embedding (eot_token is the highest number in each sequence)
         x = x[torch.arange(x.shape[0]), idx_max_n] @ self.projection
