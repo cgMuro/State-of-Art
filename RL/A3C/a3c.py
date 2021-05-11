a64782add3dc20a965cb829efc739928d77d812e
Gio <cmurogio@gmail.com>
2021-05-11 20:02:13 +0200

    A3C algorithm implementation

---
+++ RL/A3C/a3c.py
@@ -0,0 +1,144 @@
+# https://github.com/ikostrikov/pytorch-a3c
+
+import math
+from typing import Tuple
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+# ----------------------------------- MODEL ----------------------------------- #
+
+def normalized_columns_initializer(weights, std=1.0):
+    out = torch.randn(weights.size())
+    out *= std / torch.sqrt(out.pow(2).sum(1, keepdim=True))
+    return out
+
+class ActorCritic(nn.Module):
+    def __init__(self, num_inputs, action_space):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+
+        self.lstm = nn.LSTMCell(input_size=32 * 3 * 3, hidden_size=256)
+
+        num_outputs = action_space.n
+        self.critic_linear = nn.Linear(256, 1)
+        self.actor_linear = nn.Linear(256, num_outputs)
+
+        self.apply(self._init_weights)
+        self.actor_linear.weight.data = normalized_columns_initializer(self.actor_linear.weight.data, 0.01)
+        self.actor_linear.bias.data.fill_(0)
+        self.critic_linear.weight.data = normalized_columns_initializer(self.critic_linear.weight.data, 0.01)
+        self.critic_linear.bias.data.fill_(0)
+
+        self.lstm.bias_ih.data.fill_(0)
+        self.lstm.bias_hh.data.fill_(0)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Conv2d):
+            weight_shape = list(module.weight.data.size())
+            fan_in = np.prod(weight_shape[1:4])
+            fan_out = np.prod(weight_shape[2:4]) * weight_shape[0]
+            w_bound = np.sqrt(6. / (fan_in + fan_out))
+            module.weight.data.uniform_(-w_bound, w_bound)
+            module.bias.data.fill_(0)
+        elif isinstance(module, nn.Linear):
+            weight_shape = list(module.weight.data.size())
+            fan_in = weight_shape[1]
+            fan_out = weight_shape[0]
+            w_bound = np.sqrt(6. / (fan_in + fan_out))
+            module.weight.data.uniform_(-w_bound, w_bound)
+            module.bias.data.fill_(0)
+
+
+    def forward(self, inputs):
+        inputs, (hx, cx) = inputs
+        
+        x = F.elu(self.conv1(inputs))
+        x = F.elu(self.conv2(x))
+        x = F.elu(self.conv3(x))
+        x = F.elu(self.conv4(x))
+
+        x = x.view(-1, 32 * 3 * 3)
+        hx, cx = self.lstm(x, (hx, cx))
+        x = hx
+
+        return self.critic_linear(x), self.actor_linear(x), (hx, cx)
+
+
+
+# ----------------------------------- OPTIMIZATION ----------------------------------- #
+
+class SharedAdam(torch.optim.Adam):
+    """ Adam algorithm with shared states """
+    def __init__(
+        self, 
+        params, 
+        lr: float = 1e-3,
+        betas: Tuple[float, float] = (0.9, 0.999), 
+        eps: float = 1e-8, 
+        weight_decay: float = 0
+    ) -> None:
+        super().__init__(params, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'] = torch.zeros(1)
+                state['exp_avg'] = p.data.new().resize_as_(p.data).zero_()
+                state['exp_avg_sq'] = p.data.new().resize_as_(p.data).zero_()
+        
+    def share_memory(self):
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'].share_memory_()
+                state['exp_avg'].share_memory_()
+                state['exp_avg_sq'].share_memory_()
+
+    def step(self, closure=None):
+        """
+        Performs a single optimization step.
+
+        Arguments:
+            - closure (callable, optional): A closure that reevaluates the model and returns the loss.
+        """
+        
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                state = self.state[p]
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+
+                if group['weight_decay'] != 0:
+                    grad = grad.add(group['weight_decay'], p.data)
+
+                # Decay the first and second moment running average coefficient
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+
+                denom = exp_avg_sq.sqrt().add_(group['eps'])
+
+                bias_correction1 =  1 - beta1 ** state['step'].item()
+                bias_correction2 =  1 - beta2 ** state['step'].item()
+                step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
+
+                p.data.addcdiv_(-step_size, exp_avg, denom)
+
+        return loss
