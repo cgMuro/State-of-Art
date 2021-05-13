73ccefbc9c38cb7ac01924c00b47f995d7f78338
Gio <cmurogio@gmail.com>
2021-05-13 21:27:04 +0200

    A2C algorithm implementation

---
+++ RL/A2C/a2c.py
@@ -0,0 +1,56 @@
+# https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail/blob/master/a2c_ppo_acktr/algo/kfac.py
+
+import torch
+import torch.nn as nn
+
+
+class A2C():
+    def __init__(
+        self,
+        actor_critic,
+        value_loss_coef,
+        entropy_coef,
+        lr=None,
+        eps=None,
+        alpha=None,
+        max_grad_norm=None
+    ) -> None:
+
+        self.actor_critic = actor_critic
+
+        self.value_loss_coef = value_loss_coef
+        self.entropy_coef = entropy_coef
+
+        self.max_grad_norm = max_grad_norm
+
+        self.optimizer = torch.optim.RMSprop(actor_critic.parameters(), lr, eps=eps, alpha=alpha)
+
+    def update(self, rollouts):
+        obs_shape = rollouts.obs.size()[2:]
+        action_shape = rollouts.actions.size()[-1]
+        num_steps, num_processes, _ = rollouts.rewards.size()
+
+
+        values, action_log_probs, dist_entropy, _ =  self.actor_critic.evaluae_actions(
+            rollouts.obs[:-1].view(-1, *obs_shape),
+            rollouts.recurent_hidden_states[0].view(-1, self.actor_critic.recurrent_hidden_state_size),
+            rollouts.masks[:-1].view(-1, 1),
+            rollouts.actions.view(-1, action_shape)
+        )
+
+        values = values.view(num_steps, num_processes, 1)
+        action_log_probs = action_log_probs.view(num_steps, num_processes, 1)
+
+        advantages = rollouts.returns[:-1] - values
+        value_loss = advantages.pow(2).mean()
+
+        action_loss = -(advantages.detach() * action_log_probs).mean()
+
+        self.optimizer.zero_grad()
+        (value_loss * self.value_loss_coef + action_loss - dist_entropy * self.entropy_coef).backward()
+
+        nn.utils.clip_grad.clip_grad_norm_(self.actor_critic.parameters(), self.max_grad_norm)
+
+        self.optimizer.step()
+
+        return value_loss.item(), action_loss.item(), dist_entropy.item()
